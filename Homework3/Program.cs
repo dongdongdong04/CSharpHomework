067ef9b80d7ac42abaa1f0788cf10fe56d7175a9
dongdong <843024492@qq.com>
2020-03-04 14:20:40 +0800

    changed .cs for random'

--- Homework3/Program.cs
+++ Homework3/Program.cs
@@ -10,56 +10,72 @@ namespace Homework3
     {
         static void Main(string[] args)
         {
-            Init(out int input, out int num);
-            Shape[] myShape = new Shape[num];
-            double areaSum = 0;
+            bool flag = true;
 
-            switch (input)
+            while (flag)
             {
-                case 1:
-                    ManualInput(num, ref myShape);
-                    for (int i = 0; i < num; i++)
-                        areaSum += myShape[i].GetArea();
-                    Console.WriteLine("这 {0} 个图形的面积之和为：{1}", num, areaSum);
-                    Console.WriteLine("------------------------输 入 任 意 键 继 续--------------------------");
-                    break;
-
-                case 2:
-                    Console.Clear();
-                    Console.WriteLine("--------------------------随 机 生 成 图 形---------------------------");
-                    for (int i = 0; i < num; i++)
-                    {
-                        ShapeFactory shapeFactory = new ShapeFactory();
-                        Console.Write("第 {0} 个图形是", i);
-                        myShape[i] = shapeFactory.RandomShape();
-                        areaSum += myShape[i].GetArea();
-                    }
-                    Console.WriteLine("这 {0} 个图形的面积之和为：{1}", num, areaSum);
-                    Console.WriteLine("------------------------输 入 任 意 键 继 续--------------------------");
-                    break;
+                Console.Clear();
+                Console.WriteLine("---------------------------CSharpHomework3----------------------------");
+                Console.WriteLine("功能：手动或随机生成若干长方形、正方形、三角形、圆形，并输出其面积和。");
+                Console.WriteLine("1 - 人工输入      2 - 随机生成     0 - 退出程序");
+                Console.Write("请选择生成方式（请输入汉字前对应字符）：");
+                string input = Console.ReadLine();
+                int num = 0; double areaSum = 0;
+                Shape[] myShape = null;
+                switch (input)
+                {
+                    case "1":
+                        Console.Write("请选择生成图形数目（最多生成20个）：");
+                        while (!IsLegalInput(out num, 0, 20)) ;
+                        myShape = new Shape[num];
+                        ManualInput(num, ref myShape);
+                        int fail = 0;
+                        for (int i = 0; i < num; i++)
+                        {
+                            if (myShape[i].GetArea() == -1) fail++;
+                            else areaSum += myShape[i].GetArea();
+                        }
+                        Console.WriteLine("目标生成{0}个图形，其中{1}个图形创建失败，其余图形的面积之和为：{2}", num, fail, areaSum);
+                        Console.WriteLine("------------------------输 入 任 意 键 继 续--------------------------");
+                        Console.ReadKey();
+                        break;
+                    case "2":
+                        Console.WriteLine("--------------------------随 机 生 成 图 形---------------------------");
+                        Console.Write("请选择生成图形数目（最多生成20个）：");
+                        while (!IsLegalInput(out num, 0, 20)) ;
+                        myShape = new Shape[num];
+                        for (int i = 0; i < num; i++)
+                        {
+                            ShapeFactory shapeFactory = new ShapeFactory();
+                            Console.Write("第 {0} 个图形是", i + 1);
+                            myShape[i] = shapeFactory.RandomShape();
+                            areaSum += myShape[i].GetArea();
+                            System.Threading.Thread.Sleep(100);
+                        }
+                        Console.WriteLine("这 {0} 个图形的面积之和为：{1}", num, areaSum);
+                        Console.WriteLine("------------------------输 入 任 意 键 继 续--------------------------");
+                        Console.ReadKey();
+                        break;
+                    case "0":
+                        flag = false;
+                        break;
+                    default:
+                        Console.WriteLine("请重新选择！");
+                        break;
+                }
             }
-            Console.ReadKey();
-        }
-
-        //初始化
-        static void Init(out int input, out int num)
-        {
-            Console.WriteLine("---------------------------CSharpHomework3----------------------------");
-            Console.WriteLine("功能：手动或随机生成若干长方形、正方形、三角形、圆形，并输出其面积和。");
-            Console.WriteLine("1 - 人工输入      2 - 随机生成");
-            Console.Write("请选择生成方式（请输入汉字前对应字符）：");
-            while (!IsLegalInput(out input) || input != 1 && input != 2 && input != 0) ;
-            Console.Write("请输入生成个数（请输入20以内的正整数）：");
-            while (!IsLegalInput(out num) || num < 0 && num >= 20);
         }
 
         //判断输入合法性
-        static bool IsLegalInput(out int numInput)
+        static bool IsLegalInput(out int numInput, int min, int max)
         {
             bool flag;
             flag = int.TryParse(Console.ReadLine(), out numInput);
-            if (flag == false) Console.Write("输入不合法！请重新输入：");
-            else flag = true;
+            if (flag == false || numInput < min || numInput > max)
+            {
+                flag = false;
+                Console.Write("输入不合法！请重新输入：");
+            }
             return flag;
         }
         static bool IsLegalInput(out double numInput)
@@ -72,9 +88,8 @@ namespace Homework3
         }
 
         //手动输入过程
-        static void ManualInput(int num, ref Shape[] shape)
+        static void ManualInput(int num, ref Shape[] myShape)
         {
-            Console.Clear();
             Console.WriteLine("--------------------------手 动 生 成 图 形---------------------------");
             Console.WriteLine("1 - 生成长方形    2 - 生成正方形    3 - 生成三角形    4 - 生成圆形");
             ShapeFactory shapeFactory = new ShapeFactory();
@@ -82,7 +97,7 @@ namespace Homework3
             for (int i = 0; i < num; i++)
             {
                 Console.Write("这是生成的第 {0} 个图形，请选择生成图形形状：", i + 1);
-                while (!IsLegalInput(out input) || input > 4 && input < 1) ;
+                while (!IsLegalInput(out input, 0, 4)) ;
                 double[] edge= new double[3];
                 switch (input)
                 {
@@ -109,14 +124,15 @@ namespace Homework3
                         while (!IsLegalInput(out edge[0])) ;
                         break;
                 }
-                shape[i] = shapeFactory.CreateShape(input - 1, edge);
+                myShape[i] = shapeFactory.CreateShape(input - 1, edge);
+                if (!myShape[i].IsLegal()) Console.WriteLine("创建失败！");
+                else Console.WriteLine("创建成功！该图形面积为：" + myShape[i].GetArea());
             }
         }
     }
 
     class ShapeFactory
     {
-        Random r = new Random();
         private Shape myShape = null;
         public Shape CreateShape(int type, double[] args)
         {
@@ -141,25 +157,48 @@ namespace Homework3
         }
         public Shape RandomShape()
         {
-            int flag = r.Next() % 4;
-            double[] args = { r.Next(100), r.Next(100), r.Next(100) };
-            switch (flag)
+            Random r = new Random();
+            int type = r.Next() % 4;
+            switch (type)
             {
                 case 0:
-                    myShape = new Rectangle(args[0], args[1]);
-                    Console.WriteLine("长方形，长为{0}，宽为{1}", args[0], args[1]);
+                    double width, height;
+                    do
+                    {
+                        width = 10 * r.NextDouble();
+                        height = 10 * r.NextDouble();
+                        myShape = new Rectangle(width, height);
+                    } while (!myShape.IsLegal());
+                    Console.WriteLine("长方形，长为{0}，宽为{1}", width, height);
                     break;
                 case 1:
-                    myShape = new Square(args[0]);
-                    Console.WriteLine("正方形，边长为{0}", args[0]);
+                    double edge;
+                    do
+                    {
+                        edge = 10 * r.NextDouble();
+                        myShape = new Square(edge);
+                    } while (!myShape.IsLegal());
+                    Console.WriteLine("正方形，边长为{0}", edge);
                     break;
                 case 2:
-                    myShape = new Triangle(args[0], args[1], args[2]);
-                    Console.WriteLine("三角形，三边边长为{0}，{1}，{2}", args[0], args[1], args[2]);
+                    double a, b, c;
+                    do
+                    {
+                        a = 10 * r.NextDouble();
+                        b = 10 * r.NextDouble();
+                        c = 10 * r.NextDouble();
+                        myShape = new Triangle(a, b, c);
+                    } while (!myShape.IsLegal());
+                    Console.WriteLine("三角形，三边边长为{0}，{1}，{2}", a, b, c);
                     break;
                 case 3:
-                    myShape = new Circle(args[0]);
-                    Console.WriteLine("圆形，半径为{0}", args[0]);
+                    double radius;
+                    do
+                    {
+                        radius = 10 * r.NextDouble();
+                        myShape = new Circle(radius);
+                    }while (!myShape.IsLegal());
+                    Console.WriteLine("圆形，半径为{0}", radius);
                     break;
                 default:
                     break;
