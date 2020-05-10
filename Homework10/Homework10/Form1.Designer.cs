c6d64bff84bad2b6cb6c569ee1fe5504bb3a3590
dongdong <843024492@qq.com>
2020-05-10 21:53:42 +0800

    hw10

---
+++ Homework10/Homework10/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Homework10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.dgvResult = new System.Windows.Forms.DataGridView();
+            this.flowLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResult)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.txtUrl);
+            this.flowLayoutPanel1.Controls.Add(this.btnStart);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(7);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Padding = new System.Windows.Forms.Padding(49, 45, 0, 0);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(1735, 145);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(56, 52);
+            this.txtUrl.Margin = new System.Windows.Forms.Padding(7);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(805, 38);
+            this.txtUrl.TabIndex = 0;
+            this.txtUrl.Text = "http://www.cnblogs.com/dstang2000/";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(875, 52);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(7);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(197, 59);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "开  始";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click_1);
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Location = new System.Drawing.Point(14, 161);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(0, 27);
+            this.lblInfo.TabIndex = 1;
+            // 
+            // dgvResult
+            // 
+            this.dgvResult.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvResult.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvResult.Location = new System.Drawing.Point(17, 206);
+            this.dgvResult.Margin = new System.Windows.Forms.Padding(5);
+            this.dgvResult.Name = "dgvResult";
+            this.dgvResult.RowTemplate.Height = 23;
+            this.dgvResult.Size = new System.Drawing.Size(1704, 728);
+            this.dgvResult.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 27F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1735, 955);
+            this.Controls.Add(this.dgvResult);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "Form1";
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResult)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblInfo;
+        private System.Windows.Forms.DataGridView dgvResult;
+    }
+}
+
